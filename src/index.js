796bc3d548d5904931882ed962c2341f3505c230
Emilse Arias <emilsearias@gmail.com>
2022-02-09 17:52:17 -0300

    agregamos los midlewares y el index.html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
 
 //crear una instancia de express
 const app = express();
@@ -7,4 +10,19 @@ app.set('port', process.env.PORT || 4000);
 
 app.listen( app.get('port'), ()=>{
     console.log('Estoy en el puerto '+ app.get('port'));
+})
+
+//midlewares
+app.use(morgan('dev')); // da informacion en la terminal
+app.use(cors()); //permite peticiones remotas
+//permite recibir objetos en formato json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname,'../public')))
+
+//creamos una ruta de prueba
+app.get('/products',(req, res)=>{
+//aqui escribo toda la logica que quiero que suceda cuando llega un request
+ res.send('aqui enviaria una lista de productos');
 })
