509a2ecd3f5a8e26a2b845026c929e0f24f7d37e
Emilse Arias <emilsearias@gmail.com>
2022-02-21 18:29:57 -0300

    cambiamos la cadena de conexion a la BD

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://localhost:27017/bdcafe';
+// const url = 'mongodb://localhost:27017/bdcafe';
+const url = 'mongodb+srv://emic19i:C19Iemirc@cluster0.4oulm.mongodb.net/bdcafe';
 
 mongoose.connect(url);
 
